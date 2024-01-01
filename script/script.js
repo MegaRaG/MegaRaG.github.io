bd9579408aaa3fa0a7d7ba6f0c84dbac508652dd
Ahmed.O <omerovicahmed12@gmail.com>
2024-01-01 01:15:19 +0100

    Ajout du Filtre

--- script/script.js
+++ script/script.js
@@ -35,6 +35,26 @@ fetch('./python/champions_info.json')
       championsContainer.appendChild(championDiv);
     });
 
+    const filterInput = document.getElementById('filterInput');
+
+  filterInput.addEventListener('input', function() {
+    const filterValue = this.value.toLowerCase().trim(); // Convertir en minuscules pour une recherche insensible à la casse
+
+    const championItems = document.querySelectorAll('.champion-item'); // Sélectionnez tous les éléments de champion
+
+    championItems.forEach(item => {
+      const championHeaderText = item.querySelector('.list-header').textContent.toLowerCase(); // Obtenez le texte du header du champion
+      const championName = championHeaderText.split('-')[0].trim(); // Récupérez le nom du champion en ignorant les autres informations
+
+      // Si le nom du champion contient la valeur filtrée, affichez-le ; sinon, masquez-le
+      if (championName.includes(filterValue)) {
+        item.style.display = ''; // Afficher le champion
+      } else {
+        item.style.display = 'none'; // Masquer le champion
+      }
+    });
+  });
+
   })
   .catch(error => {
     console.error('Error fetching the JSON file:', error);
